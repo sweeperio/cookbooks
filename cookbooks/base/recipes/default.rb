0dbc592708c279752be2be213e6850de411fc0d7
pseudomuto <david.muto@gmail.com>
2015-08-02 11:03:34 -0400

    ensuring redis is installed

--- cookbooks/base/recipes/default.rb
+++ cookbooks/base/recipes/default.rb
@@ -5,5 +5,6 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 include_recipe "apt"
+include_recipe "build-essential"
 include_recipe "base::packages"
 include_recipe "base::chruby"
--- test/integration/apps_dude/serverspec/default_spec.rb
+++ test/integration/apps_dude/serverspec/default_spec.rb
@@ -4,4 +4,8 @@ describe "dude" do
   describe command("which redis") do
     its(:stdout) { should_not be_nil }
   end
+
+  describe command("which redis-cli") do
+    its(:stdout) { should_not be_nil }
+  end
 end
