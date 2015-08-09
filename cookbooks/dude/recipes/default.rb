a4b55e3557b151f8e5b1c6486c9277c37cd29df6
pseudomuto <david.muto@gmail.com>
2015-08-09 12:08:24 -0400

    ensuring chruby is sourced for capistrano

--- cookbooks/dude/recipes/default.rb
+++ cookbooks/dude/recipes/default.rb
@@ -4,4 +4,14 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
+ruby_block "source chruby in bashrc for deploy user" do
+  block do
+    original_content = IO.read("/home/deploy/.bashrc")
+    IO.write("/home/deploy/.bashrc", "source /etc/profile.d/chruby.sh\n#{original_content}")
+  end
+
+  action :run
+  not_if "grep -q chruby /home/deploy/.bashrc"
+end
+
 include_recipe "dude::apps"
