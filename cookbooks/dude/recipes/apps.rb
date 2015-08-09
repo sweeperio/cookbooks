00ff665a8e6fb5c2b6e82821d05bc1f0016f73d2
pseudomuto <david.muto@gmail.com>
2015-08-09 09:10:16 -0400

    delegate ejson key management to the ejson cookbook

--- cookbooks/dude/recipes/apps.rb
+++ cookbooks/dude/recipes/apps.rb
@@ -3,17 +3,17 @@
 # Recipe:: ejson
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+MISSING_KEY_MESSAGE = "One or more ejson keys are missing from node[\"ejson\"][\"keys\"][\"add\"]"
 
-ejson_keys     = Chef::EncryptedDataBagItem.load("ejson", "keys")
 deploy_tokens  = Chef::EncryptedDataBagItem.load("tokens", "github")
 deploy_account = "deploy"
 
-directory "/opt/ejson/keys" do
-  recursive true
-  owner deploy_account
-  group deploy_account
-  mode "0660"
-end
+apps      = node["apps"].map { |name| Chef::EncryptedDataBagItem.load("apps", name).to_hash }
+node_keys = node["ejson"]["keys"].fetch("add", [])
+app_keys  = apps.map { |settings| settings.fetch("ejson_keys", []) }.flatten.compact
+
+# all app ejson keys should be part of this provision
+Chef::Application.fatal!(MISSING_KEY_MESSAGE) if (app_keys - node_keys).any?
 
 directory "/home/#{deploy_account}/.ssh" do
   owner deploy_account
@@ -35,11 +35,11 @@ directory "/home/#{deploy_account}/repos" do
   notifies :run, "execute[add github.com to known_hosts]", :immediately
 end
 
-node["apps"].each do |app_name|
-  app  = Chef::EncryptedDataBagItem.load("apps", app_name).to_hash
+apps.each do |app|
+  name = app.fetch("id")
   repo = app.fetch("github_repo")
 
-  deploy_key "#{app_name}_deploy_key" do
+  deploy_key "#{name}_deploy_key" do
     provider Chef::Provider::DeployKeyGithub
     action :add
     credentials(token: deploy_tokens["sweeper-deploy"])
@@ -50,31 +50,20 @@ node["apps"].each do |app_name|
     mode "0600"
   end
 
-  template "/home/#{deploy_account}/repos/#{app_name}.sh" do
+  template "/home/#{deploy_account}/repos/#{name}.sh" do
     source "ssh_wrapper.erb"
     owner deploy_account
     group deploy_account
     mode "0750"
-    variables key: "/home/#{deploy_account}/.ssh/#{app_name}_deploy_key"
+    variables key: "/home/#{deploy_account}/.ssh/#{name}_deploy_key"
   end
 
-  git "/home/#{deploy_account}/repos/#{app_name}" do
+  git "/home/#{deploy_account}/repos/#{name}" do
     action :checkout
     repository "git@github.com:#{repo}.git"
     revision "master"
-    ssh_wrapper "/home/#{deploy_account}/repos/#{app_name}.sh"
+    ssh_wrapper "/home/#{deploy_account}/repos/#{name}.sh"
     user deploy_account
     group deploy_account
-  end
-
-  app.fetch("ejson_keys", []).each do |key|
-    data = ejson_keys[key]
-
-    file "/opt/ejson/keys/#{data['public']}" do
-      content data["private"]
-      owner deploy_account
-      group deploy_account
-      mode "0440"
-    end
   end
 end
--- cookbooks/dude/spec/unit/recipes/apps_spec.rb
+++ cookbooks/dude/spec/unit/recipes/apps_spec.rb
@@ -1,128 +1,99 @@
 #
 # Cookbook Name:: dude
-# Spec:: ejson
+# Spec:: apps
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 require "spec_helper"
 
 describe "dude::apps" do
-  before do
+  context "when app defines missing ejson key" do
+    let(:chef_run) do
+      expect(Chef::EncryptedDataBagItem).to receive(:load).with("tokens", "github").and_return(
+        "id"             => "github",
+        "sweeper-deploy" => "12345abcdefghijklmnopqrstuvwxyz"
+      )
+
+      expect(Chef::EncryptedDataBagItem).to receive(:load).with("apps", "dude").and_return(
+        "id"          => "dude",
+        "github_repo" => "sweeperio/dude",
+        "ejson_keys"  => %w(dude not_defined)
+      )
+
+      runner = ChefSpec::ServerRunner.new do |node|
+        node.set["apps"] = %w(dude)
+        node.set["ejson"]["keys"]["add"] = %w(dude)
+      end
+    end
+
+    it "gets all fatal on converge" do
+      expect { chef_run.converge(described_recipe) }.to raise_error(SystemExit)
+    end
+  end
+
+  cached(:chef_run) do
     expect(Chef::EncryptedDataBagItem).to receive(:load).with("tokens", "github").and_return(
       "id"             => "github",
       "sweeper-deploy" => "12345abcdefghijklmnopqrstuvwxyz"
     )
 
-    expect(Chef::EncryptedDataBagItem).to receive(:load).with("ejson", "keys").and_return(
-      "id" => "keys",
-      "dude" => {
-        "public" => "public_key",
-        "private" => "private_key"
-      }
+    expect(Chef::EncryptedDataBagItem).to receive(:load).with("apps", "dude").and_return(
+      "id"          => "dude",
+      "github_repo" => "sweeperio/dude",
+      "ejson_keys"  => %w(dude)
     )
-  end
 
-  let(:chef_run) do
     runner = ChefSpec::ServerRunner.new do |node|
       node.set["apps"] = %w(dude)
+      node.set["ejson"]["keys"]["add"] = %w(dude)
     end
 
     runner.converge(described_recipe)
   end
 
-  context "with app definition" do
-    before do
-      expect(Chef::EncryptedDataBagItem).to receive(:load).with("apps", "dude").and_return(
-        "id"          => "dude",
-        "github_repo" => "sweeperio/dude"
-      )
-    end
-
-    it "creates the ejson keys directory" do
-      expect(chef_run).to create_directory("/opt/ejson/keys").with(
-        recursive: true,
-        owner: "deploy",
-        group: "deploy",
-        mode: "0660"
-      )
-    end
-
-    it "creates the ssh directory" do
-      expect(chef_run).to create_directory("/home/deploy/.ssh").with(
-        owner: "deploy",
-        group: "deploy",
-        mode: "0700"
-      )
-    end
-
-    it "creates the repo folder" do
-      expect(chef_run).to create_directory("/home/deploy/repos").with(
-        owner: "deploy",
-        group: "deploy",
-        mode: "0770"
-      )
-    end
-
-    it "creates the ssh wrapper file" do
-      expect(chef_run).to create_template("/home/deploy/repos/dude.sh").with(
-        source: "ssh_wrapper.erb",
-        owner: "deploy",
-        group: "deploy",
-        mode: "0750",
-        variables: { key: "/home/deploy/.ssh/dude_deploy_key" }
-      )
-    end
-
-    it "clones the app's master branch" do
-      expect(chef_run).to checkout_git("/home/deploy/repos/dude").with(
-        repository: "git@github.com:sweeperio/dude.git",
-        revision: "master",
-        user: "deploy",
-        group: "deploy"
-      )
-    end
-
-    it "creates a deploy key for the dude repo" do
-      expect(chef_run).to add_deploy_key("dude_deploy_key").with(
-        path: "/home/deploy/.ssh",
-        credentials: { token: "12345abcdefghijklmnopqrstuvwxyz" },
-        repo: "sweeperio/dude",
-        owner: "deploy",
-        group: "deploy",
-        mode: "0600"
-      )
-    end
+  it "creates the ssh directory" do
+    expect(chef_run).to create_directory("/home/deploy/.ssh").with(
+      owner: "deploy",
+      group: "deploy",
+      mode: "0700"
+    )
   end
 
-  context "when ejson_keys are defined" do
-    before do
-      expect(Chef::EncryptedDataBagItem).to receive(:load).with("apps", "dude").and_return(
-        "id"          => "dude",
-        "github_repo" => "sweeperio/dude",
-        "ejson_keys"  => ["dude"]
-      )
-    end
-
-    it "sets the ejson file appropriately" do
-      expect(chef_run).to create_file("/opt/ejson/keys/public_key").with(
-        content: "private_key",
-        owner: "deploy",
-        group: "deploy",
-        mode: "0440"
-      )
-    end
+  it "creates the repo folder" do
+    expect(chef_run).to create_directory("/home/deploy/repos").with(
+      owner: "deploy",
+      group: "deploy",
+      mode: "0770"
+    )
   end
 
-  context "when ejson_keys are not defined" do
-    before do
-      expect(Chef::EncryptedDataBagItem).to receive(:load).with("apps", "dude").and_return(
-        "id"          => "dude",
-        "github_repo" => "sweeperio/dude"
-      )
-    end
+  it "creates the ssh wrapper file" do
+    expect(chef_run).to create_template("/home/deploy/repos/dude.sh").with(
+      source: "ssh_wrapper.erb",
+      owner: "deploy",
+      group: "deploy",
+      mode: "0750",
+      variables: { key: "/home/deploy/.ssh/dude_deploy_key" }
+    )
+  end
 
-    it "converges successfully" do
-      expect { chef_run }.to_not raise_error
-    end
+  it "clones the app's master branch" do
+    expect(chef_run).to checkout_git("/home/deploy/repos/dude").with(
+      repository: "git@github.com:sweeperio/dude.git",
+      revision: "master",
+      user: "deploy",
+      group: "deploy"
+    )
+  end
+
+  it "creates a deploy key for the dude repo" do
+    expect(chef_run).to add_deploy_key("dude_deploy_key").with(
+      path: "/home/deploy/.ssh",
+      credentials: { token: "12345abcdefghijklmnopqrstuvwxyz" },
+      repo: "sweeperio/dude",
+      owner: "deploy",
+      group: "deploy",
+      mode: "0600"
+    )
   end
 end
