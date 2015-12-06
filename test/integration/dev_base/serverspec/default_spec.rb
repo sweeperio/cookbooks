a87119eb86684fa9a77115efb5499fce57e12a5f
pseudomuto <david.muto@gmail.com>
2015-12-06 12:14:09 -0500

    cleaning up the tests a bit

--- test/integration/dev_base/serverspec/default_spec.rb
+++ test/integration/dev_base/serverspec/default_spec.rb
@@ -1,15 +1,19 @@
 require "spec_helper"
 
 describe "dev" do
-  %w(memcached phantomjs redis-server redis-cli tmux fasd).each do |app|
+  APPS     = %w(memcached phantomjs redis-server redis-cli tmux fasd node coffee grunt)
+  SERVICES = %w(memcached redis6379)
+
+  APPS.each do |app|
     describe command("which #{app}") do
       its(:stdout) { should_not eq("") }
     end
   end
 
-  %w(node coffee grunt).each do |app|
-    describe command("which #{app}") do
-      its(:stdout) { should_not eq("") }
+  SERVICES.each do |service|
+    describe service(service) do
+      it { should be_enabled }
+      it { should be_running }
     end
   end
 end
