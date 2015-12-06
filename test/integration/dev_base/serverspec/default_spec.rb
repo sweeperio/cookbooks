7bffef3fd0a4a5173ccb474ff80fa6d415a96751
pseudomuto <david.muto@gmail.com>
2015-12-06 09:22:46 -0500

    adding tmux

--- test/integration/dev_base/serverspec/default_spec.rb
+++ test/integration/dev_base/serverspec/default_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 describe "dev" do
-  %w(redis-server redis-cli).each do |app|
+  %w(redis-server redis-cli tmux).each do |app|
     describe command("which #{app}") do
       its(:stdout) { should_not eq("") }
     end
