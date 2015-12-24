5e305ed9be11baa15d212e0e8a249686d7610820
pseudomuto <david.muto@gmail.com>
2015-12-24 12:47:21 -0500

    removing unit specs...because reasons

--- test/unit/spec_helper.rb
+++
@@ -1,8 +0,0 @@
-require "chefspec"
-
-Dir["./test/unit/support/**/*.rb"].each { |f| require f }
-
-RSpec.configure do |config|
-  config.platform = "ubuntu"
-  config.version  = "14.04"
-end
--- test/unit/support/matchers.rb
+++
@@ -1,4 +0,0 @@
-# exists upstream, but not released yet
-def install_with_make_ark(resource_name)
-  ChefSpec::Matchers::ResourceMatcher.new(:ark, :install_with_make, resource_name)
-end
