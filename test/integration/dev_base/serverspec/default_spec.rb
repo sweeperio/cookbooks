3b88c4d1741358ac147592ee9c89264e575dd8e2
pseudomuto <david.muto@gmail.com>
2015-12-24 09:20:37 -0500

    adding rubocop

--- test/integration/dev_base/serverspec/default_spec.rb
+++ test/integration/dev_base/serverspec/default_spec.rb
@@ -44,11 +44,13 @@ describe "dev" do
 
     describe file("/etc/postgresql/9.3/main/pg_hba.conf") do
       it { should exist }
+      # rubocop:disable Style/RegexpLiteral
       its(:content) { should match(/^local\s*all\s*postgres\s*ident/) }
       its(:content) { should match(/^local\s*all\s*all\s*ident/) }
       its(:content) { should match(/^host\s*all\s*all\s*127.0.0.1\/32\s*md5/) }
       its(:content) { should match(/^host\s*all\s*all\s*::1\/128\s*md5/) }
       its(:content) { should match(/^host\s*all\s*all\s*192.168.0.0\/16\s*trust/) }
+      # rubocop:enable Style/RegexpLiteral
     end
   end
 
